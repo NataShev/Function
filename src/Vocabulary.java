67d593a014a657a2bb1c7693511d47fe44a23d11
Natalya Shevchenko <nsevcenko1988@gmail.com>
2022-04-25 23:22:29 +0300

    Ghanges in file

--- src/Vocabulary.java
+++ src/Vocabulary.java
@@ -1,14 +1,10 @@
-import com.sun.tools.javac.Main;
-
 import java.util.Arrays;
 import java.util.function.Function;
-import java.util.stream.Stream;
 
 public class Vocabulary {
     public static void main(String[] args) {
         String strangerWords = "абанамат фортунат бананан колистрат  панана  вокандумасунарот вокал";
-        Function<String,String[]> fun = a -> { String[] words = a.split(" ");
-            return words; };
+        Function<String, String[]> fun = a -> a.split(" ");
         Arrays.stream(fun.apply(strangerWords)).sorted().forEach(System.out::println);
     }
-    }
+}
